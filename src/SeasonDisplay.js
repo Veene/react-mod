2662b4480bf2d2e8ecc866230e6922b677ef3131
John Pawlak <johnpawlak@gmail.com>
2019-01-09 13:55:06 -0500

    initalizing state and getting location

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SeasonDisplay = () => {
+  return (
+    <div>Season Display</div>
+  )
+}
+SeasonDisplay.propTypes = {
+  latitude: PropTypes.number.isRequired
+}
+
+export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -2,10 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const App = () => {
-  return (
-    <div>Empty</div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      longitude: null,
+      latitude: null
+    }
+  }
+  
+
+  
+  // async getPosition() {
+  //   return await window.navigator.geolocation.getCurrentPosition(
+  //     position => {
+  //       console.log(position)
+  //     }, 
+  //     err => console.log(err)
+  //   )
+  // }
+  
+  render() {
+    // const position = this.getPosition().then((data) => this.state.position = data)
+    // console.log('test: ',position)
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position.coords.latitude, position.coords.longitude)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        })
+      }, 
+      err => console.log(err)
+    )
+    return (
+      <div>
+        <div>Your position is: {this.state.latitude}, {this.state.longitude}</div>
+      </div>
+    )
+  }
 }
 
 
